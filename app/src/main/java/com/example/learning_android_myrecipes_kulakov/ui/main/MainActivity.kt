02a73f5cbaf1fa985b82343dd6d05fc41cc7fd74
Alexander <alexkulakov01@gmail.com>
2023-05-11 12:47:22 +0300

    adopt layouts for landscape orientation and tablets

--- app/src/main/java/com/example/learning_android_myrecipes_kulakov/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/learning_android_myrecipes_kulakov/ui/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.learning_android_myrecipes_kulakov.ui.main
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentManager
 import com.example.learning_android_myrecipes_kulakov.R
 import com.example.learning_android_myrecipes_kulakov.ui.recipes.RecipesFragment
@@ -12,9 +14,14 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(), FragmentManager.OnBackStackChangedListener {
 
+    private var isLandscapeOrTablet = false
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        isLandscapeOrTablet = findViewById<FragmentContainerView>(R.id.fragmentContainerRight) != null
 
         val fragmentsSize = supportFragmentManager.fragments.size
         if (fragmentsSize == 0)
@@ -38,12 +45,10 @@ class MainActivity : AppCompatActivity(), FragmentManager.OnBackStackChangedList
         return false
     }
 
-    private fun isLandscapeOrTablet() = false//binding.fragmentContainerRight != null
-
     fun setFragment(fragment: Fragment) {
-        //val fragmentContainerId = if (isLandscapeOrTablet()) R.id.fragmentContainerRight else R.id.fragmentContainerLeft
+        val fragmentContainerId = if (isLandscapeOrTablet) R.id.fragmentContainerRight else R.id.fragmentContainerLeft
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainerLeft, fragment)
+            .replace(fragmentContainerId, fragment)
             .addToBackStack(null)
             .commit()
     }
